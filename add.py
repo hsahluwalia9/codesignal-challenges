518bc18255b04359b481b722bc18b9bf7ba4ce94
Harsimar Ahluwalia <Harsimar.Ahluwalia@gmail.com>
2018-10-10 19:40:34 -0400

    Added directory 'intro' which now holds all previously commited intro challenges from codesignal

--- add.py
+++
@@ -1,4 +0,0 @@
-def add(param_one, param_two):
-    return param_one + param_two
-
-print (add(1, 2))
--- century_from_year.py
+++
@@ -1,6 +0,0 @@
-import math
-
-def centuryFromYear(year):
-    if year % 100 == 0:
-        return year/100
-    return math.floor((year/100) + 1)
